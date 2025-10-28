cdca1b7b14a10955805f0da4ff907d994dd37777
Gustavo Gurgel <gurgelg15@gmail.com>
2025-10-28 13:12:52 -0300

    28/10/25-13:12:52

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,8 +6,9 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "<C-h>", "<C-w>", { noremap = true, desc = "configure ctrl + backspace to remove the last word" })
-map("t", "<Esc>", "<C-\\><C-n>", { noremap = true, desc = "exit terminal mode with <Esc>" })
+map("t", "<Esc>", "<C-\\><C-n>", { noremap = true, desc = "exit terminal mode with <A-Esc>" })
 map("n", "<leader>fs", "<cmd>AutoSession search<cr>", { desc = "search sessions" })
+map("n", "<leader>ds", "<cmd>AutoSession deletePicker<cr>", { desc = "delete session" })
 map("n", "<leader>X", "<cmd>%bd<cr>", { desc= "close all buffers"})
 map("n", "<leader>ot", "<cmd>term<cr>", { desc= "open terminal window"})
 map("n", "<leader>sc", "<cmd>set list!<cr>", { desc= "toggle show characters"})
